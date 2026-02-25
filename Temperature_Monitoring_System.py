fd0b9c2ca925b579415e3adb5875b1aac7e885ce
AbhinavSharma-AS17 <abhinav.25012002@kiet.edu>
2026-02-25 22:22:46 +0530

    Update Temperature_Monitoring_System.py

--- Temperature_Monitoring_System.py
+++ Temperature_Monitoring_System.py
@@ -1,20 +1,24 @@
 # Temperature Monitoring System
 
-
+# Input minimum and maximum temperature limits
 min_temp = int(input("Enter minimum temperature limit: "))
 max_temp = int(input("Enter maximum temperature limit: "))
 
 print("\nTemperature Monitoring Started")
 print("Enter -1 to stop the program\n")
 
+# Loop to keep checking temperature
 while True:
+    
+    # Input current temperature
     temp = int(input("Enter current temperature: "))
 
+    # Stop condition
     if temp == -1:
         print("Monitoring Stopped")
         break
 
-   
+    # Compare temperature with limits
     if temp < min_temp:
         print("Status: Temperature is BELOW minimum limit \n")
 
